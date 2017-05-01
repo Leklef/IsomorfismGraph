eb7be2d965d2232cf6b63ea0e274467ca69f66d4
Leklef <lenar8553@gmail.com>
2017-05-01 09:43:01 +0300

    Write graph class and change structure

--- src/Graph.java
+++ src/Graph.java
@@ -1,5 +1,150 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by lenar on 30.04.17.
  */
 public class Graph {
+
+    ConnectedGraph[] G;
+
+    public Graph(BitMatrix matrix) {
+        Node[] V;
+        int n = matrix.getSize();
+        V = new Node[n];
+
+        for(int i = 0; i < n; i++)
+            V[i] = new Node(i);
+        for (int i = 0; i < n; i++) {
+            int childrenSize = 0;
+
+            for (int j = 0; j < n; j++)
+                if (matrix.getBit(i,j))
+                    childrenSize++;
+            Node[] C = new Node[childrenSize];
+            int k = 0;
+
+            for (int j = 0; j < n; j++)
+                if (matrix.getBit(i,j)) {
+                    C[k] = V[j];
+                    k++;
+                }
+            V[i].addChildren(C);
+        }
+        this.G = separateIntoComponents(V);
+    }
+
+    public static ConnectedGraph[] separateIntoComponents(Node[] V) {
+        int n = V.length;
+        List<List<Node>> V1 = new ArrayList<List<Node>>();
+
+        int a = 0;
+        boolean[] visited = new boolean[n];
+        int b = 0;
+        while (b < n){
+            if (!visited[b]){
+                int[] queue = new int[n];
+                int m = b;
+                visited[m] = true;
+                queue[0] = m;
+                int l = 1;
+                int i = 0;
+
+                V1.add(new ArrayList<Node>());
+                V1.get(a).add(V[m]);
+                while (i < l) {
+                    int r = queue[i];
+                    i++;
+                    for (int j = 0; j < V[r].children.length; j++) {
+                        Node s = V[r].children[j];
+                        if (!visited[s.data]) {
+                            visited[s.data] = true;
+                            V1.get(a).add(s);
+                            queue[l] = s.data;
+                            l++;
+                        }
+                    }
+                }
+                a++;
+            }
+            b++;
+        }
+
+        ConnectedGraph[] Gfinal = new ConnectedGraph[V1.size()];
+        for (int d = 0; d < V1.size(); d++){
+            Node[] Vfinal = new Node[V1.get(d).size()];
+            for (int c = 0; c < Vfinal.length; c++){
+                Vfinal[c] = V1.get(d).get(c);
+            }
+            Gfinal[d] = new ConnectedGraph();
+            Gfinal[d].V = Vfinal;
+        }
+
+        return Gfinal;
+    }
+
+    public int getNumOfComponents() {
+        return this.G.length;
+    }
+
+    public static Map[] findIsomorphism(Graph G1, Graph G2) {
+        int n1 = G1.getNumOfComponents();
+        int n2 = G2.getNumOfComponents();
+
+        if (n1 != n2){
+            System.out.println("The number of connected components does not match!");
+            return null;
+        }
+
+        Map[] graphMap = new Map[n1 + 1];
+        graphMap[0] = new Map(n1 + 1);
+        boolean[] mapped = new boolean[n1];
+
+        int size1 = 0;
+        for (int i = 0; i < n1; i++){
+            for (int j = 0; j < G1.G[i].V.length; j++){
+                if (G1.G[i].V[j].data > size1){
+                    size1 = G1.G[i].V[j].data;
+                }
+            }
+        }
+
+        int size2 = 0;
+        for (int i = 0; i < n1; i++){
+            for (int j = 0; j < G2.G[i].V.length; j++){
+                if (G2.G[i].V[j].data > size2){
+                    size2 = G2.G[i].V[j].data;
+                }
+            }
+        }
+
+        if (size1 != size2){
+            System.out.println("The number of total nodes does not match!");
+            return null;
+        }
+
+        for (int i = 0; i < n1; i++){
+            Map map;
+            boolean flag = false;
+            for (int j = 0; j < n2; j++){
+                if (!mapped[j]){
+                    map = ConnectedGraph.areIsomorphic(G1.G[i], G2.G[j], size1+1);
+                    if (map != null && map.length != 0){
+                        flag = true;
+                        graphMap[0].add(i, i, j);
+                        graphMap[i+1] = map;
+                        mapped[j] = true;
+                        break;
+                    }
+                }
+            }
+            if (!flag) {
+                System.out.println("We could not find a map for one of the connected components!");
+                return null;
+            }
+        }
+
+        return graphMap;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("Welcome to Graph Isomorphism!");
 
-        BitMatrix adjMatrix = AdjMatrix.readAdj("input3"); //AdjMatrix.makeRandom(14);
-        BitMatrix adjMatrixPerm = AdjMatrix.readAdj("input4"); //AdjMatrix.makeRandom(14);
+        BitMatrix adjMatrix = AdjMatrix.readAdj("input1"); //AdjMatrix.makeRandom(14);
+        BitMatrix adjMatrixPerm = AdjMatrix.readAdj("input2"); //AdjMatrix.makeRandom(14);
         adjMatrix.printMatrix();
         System.out.println();
         adjMatrixPerm.printMatrix();
 
-        ConnectedGraph G1 = new ConnectedGraph(adjMatrix);
-        ConnectedGraph G2 = new ConnectedGraph(adjMatrixPerm);
+//        ConnectedGraph G1 = new ConnectedGraph(adjMatrix);
+//        ConnectedGraph G2 = new ConnectedGraph(adjMatrixPerm);
 
-        System.out.println(ConnectedGraph.areIsomorphic(G1,G2,G1.V.length));
+        Graph gr1 = new Graph(adjMatrix);
+        Graph gr2 = new Graph(adjMatrixPerm);
+
+        //System.out.println(ConnectedGraph.areIsomorphic(G1,G2,G1.V.length));
+        System.out.println(Graph.findIsomorphism(gr1, gr2));
         System.out.println("End program");
     }
 }
